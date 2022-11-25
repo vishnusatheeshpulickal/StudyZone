bcc4e3c0e43f558829bd0e509058c29c48301600
Vishnu Satheesh <vishnusatheeshpulickal555@gmail.com>
2022-11-25 23:44:16 +0530

    Welcome page is designed.

---
+++ app/config/colors.js
@@ -0,0 +1,7 @@
+export default {
+  primary: "#fc5c65",
+  secondary: "#2483c1",
+  black: "#000",
+  white: "#fff",
+  medium: "#6e6969",
+};
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+
+import AppButton from "../components/Button";
+import colors from "../config/colors";
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      blurRadius={10}
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo.png")} />
+        <Text style={styles.tagLine}>Learn in a matter of days</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <AppButton title={"login"} />
+        <AppButton title={"register"} color='secondary' />
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    padding: 20,
+    width: "100%",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  tagLine: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
+    color: colors.medium,
+  },
+});
+
+export default WelcomeScreen;
